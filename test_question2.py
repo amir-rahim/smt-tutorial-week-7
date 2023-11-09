9bda0a41d36c0bc9f4c59ac5122ae566367c7c54
Kaushik Chinta <91189866+kaushik-chinta@users.noreply.github.com>
2023-11-09 22:04:23 +0000

    Add tests for question 2

--- test_question2.py
+++ test_question2.py
@@ -6,4 +6,8 @@ class T(unittest.TestCase):
     def test_it_works_on_one_example(self):
         self.assertEqual(quick_sort([8,2,1,7,8]),[1,2,7,8,8]);
 
+    @given(st.lists(st.integers()))
+    def test_quick_sort(self, arr):
+        self.assertEqual(quick_sort(arr), sorted(arr))        
+
 unittest.main()
