44afbdeae448a3be494942c3b384f2ad19219bbc
Kaushik Chinta <91189866+kaushik-chinta@users.noreply.github.com>
2023-11-09 22:00:49 +0000

    Fix code

--- question2.py
+++ question2.py
@@ -6,13 +6,13 @@ def quick_sort(arr):
         return arr
     else:
         for i in arr:
-        if i < arr[0]:
-            less.append(i)
-        elif i < arr[0]:
-            more.append(i)
-        else:
-            pivot.append(i)
-            
+            if i < arr[0]:
+                less.append(i)
+            elif i < arr[0]:
+                more.append(i)
+            else:
+                pivot.append(i)
+
     less = quick_sort(less)
     more = quick_sort(more)
     return less + pivot + more
